3bd611af2a8c7cc3aefe790131051b264c496f1d
Shreeya2004-sudo <shreeyakrishna9@gmail.com>
2025-06-11 20:06:54 +0530

    change

--- train_model.py
+++ train_model.py
@@ -3,7 +3,7 @@ from sklearn.linear_model import LinearRegression
 import joblib
 
 # Load your dataset
-data = pd.read_csv('Indian_Food_Nutrition_Processed.csv')
+data = pd.read_csv('Indian_Food_Nutrition_Processing.csv')
 
 # Select features (remove Username if non-numeric or encode otherwise; here we omit username)
 X = data[['Age', 'Weight', 'Height']]
